61381ecc214af8a052a0887a3490ba3ab36df384
MaharjanSumit-59 <sumitmahz@gmail.com>
2026-06-29 13:43:59 +0545

    users features added

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "gmail_connected",
+        "is_staff",
+    )
+
+    search_fields = (
+        "email",
+        "username",
+    )
+
+    ordering = (
+        "email",
+    )
